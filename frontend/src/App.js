afb581e55a76e5e9027ee35bc9c5871da6af7a6d
Przemek Sech <przemek.sech@ansarada.com>
2021-01-01 17:47:25 +1100

    [ADD-AUTH] Add Auth0 login and logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <LoginButton />
+        <LogoutButton />
         <img src={logo} className="App-logo" alt="logo" data-testid="logo" />
+        <Profile />
       </header>
     </div>
   );
