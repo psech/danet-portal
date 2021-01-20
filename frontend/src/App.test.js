0fb97256e6caaaef640487bd0b5989e1bdc6a109
Przemek Sech <przemek.sech@ansarada.com>
2021-01-20 12:10:09 +1100

    [ADD-ROUTES] Fix unit tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { Router } from "react-router-dom";
+import history from "./utils/history";
 
 it("renders logo", () => {
-  render(<App />);
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
   const navbar = screen.getByTestId("navigation-bar");
   expect(navbar).toBeInTheDocument();
 });
