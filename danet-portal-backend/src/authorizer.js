a4c3016ba22f38634b81b68d6bb30636653ee405
Przemek Sech <przemek.sech@ansarada.com>
2021-02-15 15:07:56 +1100

    [DP-1] Remove redundant logging

--- danet-portal-backend/src/authorizer.js
+++ danet-portal-backend/src/authorizer.js
@@ -2,8 +2,6 @@ const authorizer = require("./utils/authorizer-helper");
 const logger = require("./utils/logger");
 
 module.exports.handler = async (event) => {
-  console.log("authorizer.handler has been called");
-
   try {
     return await authorizer.authenticate(event);
   } catch (err) {
